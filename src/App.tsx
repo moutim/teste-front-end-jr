57176cb0daa51702bd2085fc7d97e2918a2a9a58
Vitor Moutim <moutimg@gmail.com>
2024-06-30 00:42:02 -0300

    feat: cria componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.scss';
+import './app.scss';
 import Home from './pages/Home/Home';
+import Header from './components/Header/Header';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <main className='limiter'>
+      <Header />
+      <main>
         <Routes>
           <Route path="/" element={ <Home /> } />
         </Routes>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { 
+  PiShieldCheckBold,
+  PiTruckBold, 
+  PiCreditCardBold,
+  PiShoppingCart,
+  PiCrownSimple
+} from 'react-icons/pi';
+import { IoIosSearch } from 'react-icons/io';
+import { IoHeartOutline } from 'react-icons/io5';
+import { PiUserCircleLight } from 'react-icons/pi';
+import { BsBoxSeam } from 'react-icons/bs';
+import './header.scss';
+import logo from '../../assets/logo.svg';
+
+const Header: React.FC = () => {
+  return (
+    <header className='limiter'>
+      <div className='header-infos'>
+        <ul>
+          <li>
+            <i><PiShieldCheckBold /></i>
+            <p>Compra <span>100% segura</span></p>
+          </li>
+          
+          <li>
+            <i><PiTruckBold /></i>
+            <p><span>Frete grátis</span> acima de R$ 200</p>
+          </li>
+
+          <li>
+            <i><PiCreditCardBold /></i>
+            <p><span>Parcele</span> suas compras</p>
+          </li>
+        </ul>
+      </div>
+
+      <div className='header-search'>
+        <div className='header-logo'>
+          <img src={logo} alt="Logo da VTEX" />
+        </div>
+
+        <div className='header-input'>
+          <input type="text" placeholder='O que você está buscando?' />
+          <button><i><IoIosSearch /></i></button>
+        </div>
+
+        <div className='header-buttons'>
+          <button><i><BsBoxSeam /></i></button>
+          <button><i><IoHeartOutline /></i></button>
+          <button><i><PiUserCircleLight /></i></button>
+          <button><i><PiShoppingCart /></i></button>
+        </div>
+      </div>
+      
+      <nav>
+        <ul>
+          <li><a href="">Todas as categorias</a></li>
+          <li><a href="">Supermercado</a></li>
+          <li><a href="">Livros</a></li>
+          <li><a href="">Moda</a></li>
+          <li><a href="">Lançamentos</a></li>
+          <li><a href=""><span>Ofertas do dia</span></a></li>
+          <li><i><PiCrownSimple /></i><a href="">Assinatura</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
